b7865dbcd6774da0d39f66ba7cee2714e7d2a55c
MariannaHon <imarianna96i@gmail.com>
2024-02-12 14:35:54 +0100

    commit-2
    after changes

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,17 @@
 class Storage {
     #items;
 
-    constructor({items}) {
+    constructor(items) {
         this.#items = items;
     }
-
     getItems() {
         return this.#items;
     }
-
     addItem(newItem) {
         this.#items.push(newItem);
     }
-
     removeItem(itemToRemove) {
-        this.#items = this.#items.filter(item => item !== itemToRemove);
+        this.#items = this.#items.filter((item) => item !== itemToRemove);
     }
 }
 
